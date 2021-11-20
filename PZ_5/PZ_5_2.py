0b65e13e9bba40a10869773229533f49637addb7
Student <gayworon4@gmail.com>
2021-11-20 13:59:14 +0300

    Начало 5 PZ

--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -2,3 +2,18 @@
 # формуле AB = exp(B*In(A)) (параметры A и B - вещественные).В случае нулевого
 # или отрицательного параметра A функция возвращает 0.С помощью этой функции
 # найти степени A^P,B^P,C^P, если даны числа P,A,B,C.
+import math
+
+
+def power1(a, b):  # Функция
+    ab = float(math.exp(b*math.log(a, math.e)))
+    return ab
+
+
+A = float(input('Значение A: '))  # Ввод переменных
+B = float(input('Значение B: '))
+C = float(input('Значение C: '))
+P = float(input('Степень A,B,C: '))
+print(power1(A, P))  # Вывод результатов
+print(power1(B, P))
+print(power1(C, P))

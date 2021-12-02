64be1fd2501cf8374bc02a9956b0ee11e5e618a8
Student <gayworon4@gmail.com>
2021-12-02 14:34:08 +0300

    small commit

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -3,4 +3,27 @@
 import random
 
 
+a = 0
+N = int(input('Размер списка: '))
+t = int(N)
 A = []
+p = N
+N -= 1
+while t:
+    A.append(random.randint(-100, 100))
+    t -= 1
+print(A)
+while p:
+    if a <= N:
+        print(A[a])
+        a += 1
+    if a <= N:
+        print(A[a])
+        a += 1
+    if a <= N:
+        print(A[N])
+        N -= 1
+    if a <= N:
+        print(A[N])
+        N -= 1
+    p -= 1

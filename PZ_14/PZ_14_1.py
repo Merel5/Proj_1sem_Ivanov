e968a9146c8cebf62876300b1ccec5799901acbd
Merel5 <Gayworon4@gmail.com>
2022-03-25 17:00:29 +0300

    Update

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,9 +1,9 @@
 # Из исходного текстового файла (price.txt) выбрать все цены. Посчитать количество полученных элементов.
 import re
 
-p = re.compile(r"[0-9]+ руб\. [0-9]+ коп\.")
+p = re.compile(r"[0-9]+ руб\. [0-9]+ коп\.")  # Поиск цен
 with open('price.txt', 'r', encoding="utf-8") as file:
     text = file.read()
     i = len(re.findall(p, text))
-print(re.findall(p, text))
+print(re.findall(p, text))  # Вывод результата
 print(i)

426181cd048800524fb3ef97b7856dfd6c964a1f
Merel5 <gayworon4@gmail.com>
2022-03-29 17:00:43 +0300

    Update

--- PZ_15/PZ_15_1.py
+++ PZ_15/PZ_15_1.py
@@ -2,32 +2,9 @@
 # Выполнить замену элементов третьего столбца исходной матрицы на полученные суммы.
 import random
 
-
-def q():
-    for i in range(row):
-        for j in range(column):
-            print(matrix[i][j], end=" ")
-        print()
-
-
-row = int(input("Введите количество строк: "))
+row = int(input("Введите количество строк: "))  # Входные данные
 column = int(input("Введите количество столбцов (минимум 3): "))
-matrix = []
-matrix2 = []
-for i in range(row):
-    a = []
-    for j in range(column):
-        a.append(random.randint(-50, 50))
-    matrix.append(a)
-q()
-for i in range(row):
-    b = 0
-    for j in range(column):
-        b += matrix[i][j]
-    matrix2.append(b)
-print(matrix2)
-for i in range(row):
-    for j in range(column):
-        if j == 2:
-            matrix[i][j] = matrix2[i]
-q()
+matrix = [[random.randint(-90, 90) for i in range(column)] for j in range(row)]
+print(matrix)
+s = [sum(matrix[i]) for i in range(column)]
+print(s)
--- PZ_15/PZ_15_2.py
+++ PZ_15/PZ_15_2.py
@@ -1,21 +1,21 @@
-# В матрице найти сумму элементов второй половины матрицы.
+# В матрице найти сумму элементов второй половины матрицы.(столбцы)
 import random
 
-row = int(input("Введите количество строк: "))
+row = int(input("Введите количество строк: "))  # Входные данные
 column = int(input("Введите количество столбцов: "))
 matrix = []
 v = column/2
 b = 0
-for i in range(row):
+for i in range(row):  # Заполнение матрицы
     a = []
     for j in range(column):
         a.append(random.randint(-50, 50))
     matrix.append(a)
-for i in range(row):
+for i in range(row):  # Вывод матрицы
     for j in range(column):
         print(matrix[i][j], end=" ")
     print()
-for i in range(row):
+for i in range(row):  # Нахождение суммы элементов второй половины матрицы
     for j in range(column):
         if j >= v:
             b += matrix[i][j]

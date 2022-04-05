4d313c47ebd983890a2177511f4ce298a981717b
Merel5 <Gayworon4@gmail.com>
2022-04-05 22:01:59 +0300

    Update

--- PZ_15/PZ_15_1.py
+++ PZ_15/PZ_15_1.py
@@ -4,7 +4,10 @@ import random
 
 row = int(input("Введите количество строк: "))  # Входные данные
 column = int(input("Введите количество столбцов (минимум 3): "))
-matrix = [[random.randint(-90, 90) for i in range(column)] for j in range(row)]
-print(matrix)
-s = [sum(matrix[i]) for i in range(column)]
-print(s)
+matrix = [[random.randint(-90, 90) for i in range(column)] for j in range(row)]  # Задаём матрицу
+print("Матрица: ", matrix)
+s = [sum(matrix[j]) for j in range(row)]  # Находим суммы строк
+print("Суммы строк: ", s)
+a = [matrix[j].insert(2, s[j]) for j in range(row)]  # Редактируем матрицу
+b = [matrix[j].pop(3) for j in range(row)]
+print("Отредактированная матрица: ", matrix)
--- PZ_15/PZ_15_2.py
+++ PZ_15/PZ_15_2.py
@@ -1,22 +1,11 @@
-# В матрице найти сумму элементов второй половины матрицы.(столбцы)
+# В матрице найти сумму элементов второй половины матрицы.
 import random
 
 row = int(input("Введите количество строк: "))  # Входные данные
 column = int(input("Введите количество столбцов: "))
-matrix = []
-v = column/2
-b = 0
-for i in range(row):  # Заполнение матрицы
-    a = []
-    for j in range(column):
-        a.append(random.randint(-50, 50))
-    matrix.append(a)
-for i in range(row):  # Вывод матрицы
-    for j in range(column):
-        print(matrix[i][j], end=" ")
-    print()
-for i in range(row):  # Нахождение суммы элементов второй половины матрицы
-    for j in range(column):
-        if j >= v:
-            b += matrix[i][j]
+v = row/2
+matrix = [[random.randint(-90, 90) for i in range(column)] for j in range(row)]  # Задаём матрицу
+print("Матрица: ", matrix)
+a = [sum(matrix[j]) for j in range(row) if j >= v]  # Находим сумму второй половины матрицы
+b = sum(a)
 print("Сумма элементов второй половины матрицы: ", b)

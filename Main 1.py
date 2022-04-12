f6b6a4ad3f714ed43b980ca0e59b7fe0a37569c8
Merel5 <gayworon4@gmail.com>
2022-04-12 16:33:43 +0300

    Update

--- Main 1.py
+++ Main 1.py
@@ -1,6 +1,8 @@
 import figures
+from figures import circle_perimeter
 print(dir(figures))
 figures.circle_area()
 figures.square_perimeter()
 figures.square_area()
 figures.triangle_area()
+circle_perimeter()

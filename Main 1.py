0540b3959a76166879497f402435ff84a1f061a3
Merel5 <gayworon4@gmail.com>
2022-04-13 13:17:10 +0300

    Update

--- Main 1.py
+++ Main 1.py
@@ -1,8 +1,10 @@
 import figures
-from figures import circle_perimeter
+from figures import triangle_perimeter
 print(dir(figures))
 figures.circle_area()
 figures.square_perimeter()
 figures.square_area()
 figures.triangle_area()
-circle_perimeter()
+triangle_perimeter()
+r = int(input("Ввести радиус круга: "))
+figures.circle_perimeter(r)

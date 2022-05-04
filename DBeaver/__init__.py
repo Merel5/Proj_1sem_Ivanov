c557c06fe53c23064304a8de2489151bb566c2fd
Merel5 <gayworon4@gmail.com>
2022-05-04 14:38:20 +0300

    Update

--- DBeaver/__init__.py
+++ DBeaver/__init__.py
@@ -1,5 +1,12 @@
 import sqlite3 as sq
 
+info_users = [
+    (1, 'Алексей', 1, 22, 1000),
+    (2, 'Миша', 1, 19, 800),
+    (3, 'Сергей', 1, 19, 900),
+    (4, 'Мария', 2, 18, 1500),
+    (5, 'Александр', 1, 20, 1100)
+]
 with sq.connect('saper.db') as con:
     cur = con.cursor()
     # cur.execute("DROP TABLE IF EXISTS users")
@@ -10,3 +17,4 @@ with sq.connect('saper.db') as con:
     old INTEGER,
     score INTEGER   
     )""")
+    cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?, ?)", info_users)
